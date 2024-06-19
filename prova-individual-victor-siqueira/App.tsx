198b70cecee6b91cf16da2fded527d9f99462af2
Victor Siqueira <vg_siqueira@hotmail.com>
2024-06-19 15:28:31 -0300

    fix: desativado status bar para melhor visualização do projeto

--- prova-individual-victor-siqueira/App.tsx
+++ prova-individual-victor-siqueira/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { StyleSheet, View } from 'react-native';
 import Header from './src/components/Header';
 import SearchBox from './src/components/SearchBox';
 import Body from './src/components/Body';
 
 export default function App() {
+  StatusBar.setHidden(true);
+
   return (
     <View style={styles.container}>
       <Header />
